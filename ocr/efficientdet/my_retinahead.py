4043db838fcb7613144ac13c131af5a6a7e638f3
Ivan Hahanov <ivanhahanov@icloud.com>
2020-05-30 20:04:47 +0300

    minor fix

--- ocr/efficientdet/my_retinahead.py
+++ ocr/efficientdet/my_retinahead.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+import numpy as np
 
 
 class DoubleConv(nn.Module):
@@ -28,15 +29,55 @@ class RetinaHead(nn.Module):
         super().__init__()
         self.anchor_ratios = anchor_ratios
         self.num_classes = num_classes
-        self.double_conv = DoubleConv(in_channels, 128)
-        self.out_class_conv = nn.Conv2d(128, self.num_classes * len(anchor_ratios), 3, 1, 1)
+        self.class_branch = nn.Sequential(
+            DoubleConv(in_channels, 128),
+            nn.Conv2d(128, num_classes * len(anchor_ratios), 3, 1, 1),
+            nn.Sigmoid()
+        )
+        self.boxes_branch = nn.Sequential(
+            DoubleConv(in_channels, 128),
+            nn.Conv2d(128, 4 * len(anchor_ratios), 3, 1, 1),
+        )
 
-    def forward_single(self, x):
-        x = self.double_conv(x)
-        x = self.out_class_conv(x)
-        x = torch.sigmoid(x)
-        return x.view(x.shape[0], -1, self.num_classes)
+    def forward_single(self, x, img_shape):
+        classes = self.class_branch(x)
+        classes = classes.view(x.shape[0], -1, self.num_classes)
+
+        boxes = self.boxes_branch(x)
+        boxes = boxes.permute(0, 2, 3, 1)
+        boxes[..., 0::4] = torch.sigmoid(boxes[..., 0::4])
+        boxes[..., 1::4] = torch.sigmoid(boxes[..., 1::4])
+
+        cell_shape = img_shape // np.array(list(boxes.shape[1:3]))
+        anchors = []
+        for ratio in self.anchor_ratios:
+            anchor = [max(cell_shape), max(cell_shape)]
+            if ratio < 1:
+                anchor[0] /= ratio
+            else:
+                anchor[1] *= ratio
+            anchors.append(anchor)
+
+        output_boxes = boxes.clone()
+        output_boxes[..., 0::4] = output_boxes[..., 0::4] * cell_shape[1] + \
+                                  torch.arange(0, boxes.shape[2]).repeat(boxes.shape[1], 1) \
+                                      .view(1, boxes.shape[1], boxes.shape[2], 1) * cell_shape[1]
+        output_boxes[..., 1::4] = output_boxes[..., 1::4] * cell_shape[0] + \
+                                  torch.arange(0, boxes.shape[1]).repeat(boxes.shape[2], 1).t() \
+                                      .view(1, boxes.shape[1], boxes.shape[2], 1) * cell_shape[0]
+        for i, anchor in enumerate(anchors):
+            output_boxes[..., (i * 4) + 2] = torch.exp(output_boxes[..., (i * 4) + 2]) * anchor[1]
+            output_boxes[..., (i * 4) + 3] = torch.exp(output_boxes[..., (i * 4) + 3]) * anchor[0]
+
+        output_boxes = output_boxes.contiguous().view(output_boxes.size(0), -1, 4)
+        train_boxes = boxes.contiguous().view(output_boxes.size(0), -1, 4)
+
+        return classes, train_boxes, output_boxes
 
     def forward(self, features, img_shape):
         # return [self.forward_single(x) for x in features][0]
-        return torch.cat([self.forward_single(x) for x in features], 1)
+        classes, train_boxes, output_boxes = list(zip(*[self.forward_single(x, img_shape) for x in features[:1]]))
+        classes = torch.cat(classes, 1)
+        train_boxes = torch.cat(train_boxes, 1)
+        output_boxes = torch.cat(output_boxes, 1)
+        return classes, train_boxes, output_boxes
--- ocr/efficientdet/train.py
+++ ocr/efficientdet/train.py
@@ -24,25 +24,25 @@ if __name__ == '__main__':
     for e in range(args.epochs):
         optimizer.zero_grad()
         img = torch.zeros((1, 3, 128, 128))
-        img[..., 7:12, 7:12] = 1
+        img[..., 32:36, 32:36] = 1
         annot = torch.FloatTensor([[7, 12, 7, 12, 0]])
         rects, classes = build_label(annot, img.shape[2:], [0.5, 1, 2], 2)
         # classes = torch.zeros((1, 1, 16, 16))
-        # classes[..., 2:3, 2:3] = 1
+        # classes[..., 3:4, 3:4] = 1
         # classes_, train_rects_, output_rects_ = model(img)
-        classes_ = model(img)
+        classes_, train_boxes, output_boxes = model(img)
         loss = focal_loss(0.25, 2)(classes_, classes)
         # out_classes, out_rects = postprocess(classes_, output_rects_)
-        # img = np.zeros((128, 128))
-        # for rect in np.unique(rects.numpy().astype(int), axis=0):
-        #     x1, y1, x2, y2 = rect
-        #     img[y1:y2, x1:x2] = 1
+        img = np.zeros((128, 128))
+        for rect in np.unique(rects.numpy().astype(int), axis=0):
+            x1, y1, x2, y2 = rect
+            img[y1:y2, x1:x2] = 1
         if e % 40 == 0:
-            # plt.imshow(classes_[0, 0].data.numpy())
-            # plt.show()
+            plt.imshow(classes_[0, 0].data.numpy())
+            plt.show()
 
         # loss = calc_loss(train_rects_, classes_, rects.view(1, *rects.shape), classes.view(1, *classes.shape))
-        # print('class idx:', classes_[..., 0].argmax(), 'annot class idx:', classes[..., 0].argmax())
+        #     print('class idx:', classes_[..., 0].argmax(), 'annot class idx:', classes[..., 0].argmax())
             print('loss:', loss.item())
 
         loss.backward()
